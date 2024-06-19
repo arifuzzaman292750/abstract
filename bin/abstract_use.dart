1f19050a8b2079b5b60dddf3277800747c4f96fc
Shakil <shakilcatapol10@gmail.com>
2024-06-19 22:17:03 +0600

    hello

---
+++ bin/abstract_use.dart
@@ -0,0 +1,23 @@
+abstract class Father{
+
+  BaperTaka(){
+    print("Toatl taka= 80000000");
+  }
+
+}
+
+class Son extends Father{
+
+//OverRidding
+  BaperTaka(){
+    print("Toatl taka= 50000000");
+  }
+
+}
+
+void main (){
+
+  var Sonobj= Son();
+  Sonobj.BaperTaka();
+
+}
